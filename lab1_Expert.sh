bc46be5a0e932542f7bbe66705d8d7c56e7826e5
Sokolova Olesia Iurevna <sokolova@ccfit.nsu.ru>
2018-11-30 18:46:18 +0700

    lab1 level expert

---
+++ lab1_Expert.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+echo "1. Найдем количество упоминаний слова Friday в большом текстовом файле"
+wget http://factorized.net/crusoe.txt
+echo "(Количество упоминаний равно 188)"
+echo "2.Заменим все слова Friday на Saturday"
+sed -i s/Friday/Saturday/g crusoe.txt
+echo "3.Программа подсчета суммы чисел по столбцам"
+echo '1 2
+2 2
+3 7' > input.txt
+echo "результат:"
+awk '{
+s1+=$1
+s2+=$2
+}
+END{
+print s1, s2
+}' input.txt
+
+
